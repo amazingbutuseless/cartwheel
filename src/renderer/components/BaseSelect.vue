1c64f9800773d2dcd42eff5f9f0e4d1320753b54
oi73ooh <cindian@amazingbutuseless.net>
2020-06-06 00:23:27 +0900

    BaseSelect 컴포넌트 리팩토링

--- src/renderer/components/BaseSelect.vue
+++ src/renderer/components/BaseSelect.vue
@@ -1,65 +1,74 @@
 <template>
-<div class="custom-select-wrapper">
-    <select
-    :name="name"
-    @change="onOptionChange"
->
-    <option
-        v-for="option in options"
-        :key="option.value"
-        :value="option.value"
-    >
-        {{ option.key }}
-    </option>
-</select>
-</div>
+    <div class="custom-select-wrapper">
+        <select
+            :name="name"
+            @change="onOptionChange"
+        >
+            <option
+                v-for="option in options"
+                :key="option.value"
+                :value="option.value"
+            >
+                {{ option.key }}
+            </option>
+        </select>
+    </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
 
-export default Vue.extend({
-    props: {
-        name: String,
-        options: {
-            type: Array,
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        props: {
+            name: String,
+            options: {
+                type: Array,
+                default: [],
+            },
+            onOptionChange: Function,
         },
-        onOptionChange: Function,
-    },
-});
+    });
 </script>
+
 
 <style lang="scss" scoped>
     @import '../scss/variables';
     @import '../scss/font';
 
-.custom-select-wrapper {
-    position: relative;
-    min-width: 20rem;
-    border: 1px solid $border-color;
-    border-radius: 2px;
-    cursor: pointer;
-  
     select {
-      padding: 1.2rem 1rem;
-      width: 100%;
-      border: 1px solid transparent;
-      background-color: transparent;
-      font-size: 1.4rem;
-      appearance: none;
-      -webkit-appearance: none;
-      outline: none;
+        padding: 1.2rem 1rem;
+        width: 100%;
+        border: 1px solid transparent;
+        background-color: transparent;
+        font-size: 1.4rem;
+        color: map-get($color, font);
+        appearance: none;
+        -webkit-appearance: none;
+        outline: none;
     }
-  
-    &:after {
-      @include FontAwesomeIcon('\f0d7');
 
-      position: absolute;
-      top: 1.2rem;
-      right: 1.2rem;
-      font-size: 1.6rem;
-      color: darken($border-color, 25%);
-      pointer-events: none;
+    .custom-select-wrapper {
+        position: relative;
+        border: 1px solid map-get($map: $color, $key: border);
+        border-radius: 2px;
+        cursor: pointer;
+    
+        &:after {
+            @include FontAwesomeIcon('\f0d7');
+
+            position: absolute;
+            top: 1.2rem;
+            right: 1.2rem;
+            font-size: 1.6rem;
+            color: darken(map-get($map: $color, $key: font), 25%);
+            pointer-events: none;
+        }
+
+        @media (min-width: map-get($breakpoints, medium)) {
+            display: inline-block;
+            min-width: 20rem;
+            align-self: center;
+        }
     }
-  }
 </style>
