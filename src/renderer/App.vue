3c24e8e756d48f48b25230e8fc5853083631b49f
oi73ooh <cindian@amazingbutuseless.net>
2020-06-06 00:21:13 +0900

    Vuex를 이용해서 state를 관리하도록 수정

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,23 +1,62 @@
 <template>
     <div>
-        <app-header></app-header>
-        <websie-container></websie-container>
+        <app-header />
+        <website-container />
     </div>
 </template>  
 
-<script lang="ts">
-import Vue from 'vue';
-import AppHeader from './components/AppHeader.vue';
-import WebsiteContainer from './components/WebsiteContainer.vue';
 
-export default Vue.extend({
-    name: 'App',
-    components: {
-        AppHeader,
-        WebsiteContainer,
-    } 
-});
+<script lang="ts">
+    import Vue from 'vue';
+    import Vuex from 'vuex';
+
+    import AppHeader from './components/AppHeader.vue';
+    import WebsiteContainer from './components/WebsiteContainer.vue';
+
+    Vue.use(Vuex);
+
+    const store = new Vuex.Store({
+        state: {
+            availableWebsites: [
+                {
+                    key: '[ORC] local',
+                    value: 'https://local.oxfordreadingclub.com',
+                },
+                {
+                    key: '[ORC] stage',
+                    value: 'https://stage.oxfordreadingclub.com',
+                }
+            ],
+            selectedWebsite: '',
+
+            availableTimestamps: [],
+            selectedTimestamp: '',
+        },
+
+        mutations: {
+            selectWebsite(state, hostname) {
+                //todo: getAvailableTimestamp
+                state.selectedWebsite = hostname;
+            },
+
+            selectTimestamp(state, timestamp) {
+                state.selectedTimestamp = timestamp;
+            },
+        },
+    });
+
+    export default Vue.extend({
+        name: 'App',
+
+        components: {
+            AppHeader,
+            WebsiteContainer,
+        },
+
+        store,
+    });
 </script>
+
 
 <style lang="scss">
     @import './scss/global';
--- src/renderer/components/AppHeader.vue
+++ src/renderer/components/AppHeader.vue
@@ -10,57 +10,59 @@
     </header>
 </template>
 
+
 <script lang="ts">
-import Vue from 'vue';
+    import Vue from 'vue';
 
-import BaseSelect from './BaseSelect.vue';
+    import BaseSelect from './BaseSelect.vue';
 
-export default Vue.extend({
-    name: 'AppHeader',
+    export default Vue.extend({
+        name: 'AppHeader',
 
-    components: {
-        BaseSelect,
-    },
+        components: {
+            BaseSelect,
+        },
 
-    methods: {
-        onWebsiteSelect(e: Event) {
-            console.log(e.target.value);
-        }
-    },
+        methods: {
+            onWebsiteSelect(e: Event) {
+                this.$store.commit('selectWebsite', e.target.value);
+            }
+        },
 
-    data: () => {
-        return {
-            'registeredWebsites': [{
-                key: '[ORC] local',
-                value: 'https://local.oxfordreadingclub.com',
+        computed: {
+            registeredWebsites() {
+                return this.$store.state.availableWebsites;
             },
-            {
-                key: '[ORC] stage',
-                value: 'https://stage.oxfordreadingclub.com',
-            }],
-        }
-    }
-});
+        },
+    });
 </script>
+
 
 <style scoped lang="scss">
     @import '../scss/variables';
 
-header {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    padding: .8rem 2.4rem;
-    border-bottom: 1px solid $border-color;
-    box-sizing: border-box;
+    header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        padding: .8rem 2.4rem;
+        border-bottom: 1px solid map-get($map: $color, $key: border);
+        background-color: map-get($color, background);
+        box-sizing: border-box;
+
+        @media (min-width: map-get($breakpoints, medium)) {
+            display: grid;
+            grid-template-columns: 1fr minmax(20rem, 25vw);
+        }
+    }
 
     h1 {
         display: inline-block;
         margin: 0;
         padding: 1rem 0;
         font-size: 1.6rem;
-        color: $primary-color;
+        color: map-get($map: $color, $key: primary);
         line-height: 1.2;
         text-align: center;
         white-space: nowrap;
@@ -70,25 +72,9 @@ header {
             font-size: 4rem;
             content: '\1F938\1F440\1F44C';
         }
-    }
 
-    @media (min-width: 40rem) {
-        display: grid;
-        grid-template-columns: 1fr minmax(20rem, 25vw);
-
-        h1 {
+        @media (min-width: map-get($breakpoints, medium)) {
             justify-self: start;
         }
-
-        .custom-select-wrapper {
-            display: inline-block;
-            min-width: auto;
-            align-self: center;
-        }
     }
-
-    @media (min-width: 102.4rem) {
-
-    }
-}
 </style>
